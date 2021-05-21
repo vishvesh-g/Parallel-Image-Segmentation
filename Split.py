92ba32a16517247b450f08e850fa2f81c4fc5ded
Vishvesh Gupta <46104505+vishvesh-g@users.noreply.github.com>
2021-05-21 23:34:20 +0530

    Delete Split.py

--- Split.py
+++
@@ -1,7 +0,0 @@
-import image_slicer
-
-def callThis():
-    tiles = image_slicer.slice('image.png', 4, save=False)
-    print('Slicing')
-    image_slicer.save_tiles(tiles, prefix='slice', format='png')
-    print('Saving')
